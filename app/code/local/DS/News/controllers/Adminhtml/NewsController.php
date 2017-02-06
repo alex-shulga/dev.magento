8d170f3ed4b30f3397ab362d36846b9009f3395c
alex-shulga <alex.shulga@swanlogic.com>
2017-02-06 19:11:45 +0200

    created admin block for News

---
+++ app/code/local/DS/News/controllers/Adminhtml/NewsController.php
@@ -0,0 +1,11 @@
+<?php
+
+class DS_News_Adminhtml_NewsController extends Mage_Adminhtml_Controller_Action
+{
+
+    public function indexAction()
+    {
+        echo '<h1>News Module: Admin section</h1>';
+    }
+
+}
